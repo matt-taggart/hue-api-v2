f26e4a1ee16dfeeefa48d4730a1d9057c00cd1e4
Matthew Taggart <matthew.taggart@cardcash.com>
2018-01-21 16:07:14 -0500

    Wrote method & tests for retrieving an individual light.

--- src/__tests__/methods.js
+++ src/__tests__/methods.js
@@ -1,10 +1,15 @@
-describe('Should test methods on Hue instance',  () => {
+import Hue from './../index';
+import dotenv from 'dotenv';
+
+dotenv.load();
+
+describe('Should test methods on Hue instance', () => {
   it('Should fail to get all lights', async () => {
     const hue = {
       getAllLights: jest.fn(() => { 
         throw new Error('Hue API Error: Invalid protocol.') 
-      })
-    }
+      }),
+    };
 
     try {
       await hue.getAllLights();
@@ -19,13 +24,41 @@ describe('Should test methods on Hue instance',  () => {
         '1': { name: 'Hue color lamp 1' },
         '2': { name: 'Hue color lamp 2' },
         '3': { name: 'Hue color lamp 3' },
-      }))
-    }
+      })),
+    };
 
     const result = await hue.getAllLights();
 
     expect(result['1'].name).toEqual('Hue color lamp 1');
     expect(result['2'].name).toEqual('Hue color lamp 2');
     expect(result['3'].name).toEqual('Hue color lamp 3');
+  });
+
+  it('Should fail to get light due to missing id paramter', async () => {
+    const hue = {
+      getLight: jest.fn(id => { 
+        if (!id) {
+          throw new TypeError('getLight(id): id parameter is missing.') 
+        }
+      }),
+    };
+
+    try {
+      await hue.getLight();
+    } catch (error) {
+      expect(error.message).toEqual('getLight(id): id parameter is missing.');
+    }
+  })
+
+  it('Should get individual light', async () => {
+    const hue = {
+      getLight: jest.fn(id => Promise.resolve({
+        name: 'Hue color lamp 1',
+      })),
+    }
+    
+    const light = await hue.getLight('1');
+
+    expect(light.name).toEqual('Hue color lamp 1');
   });
 })
--- src/instance/index.js
+++ src/instance/index.js
@@ -14,6 +14,19 @@ class Hue {
       throw new Error(`Hue API Error: ${error.message}`);
     }
   }
+  async getLight(id) {
+    if (!id) {
+      throw new TypeError('getLight(id): id parameter is missing.')
+    }
+
+    try {
+      const httpRequest = await fetch(`http://${this.ip}/api/${this.username}/lights/${id}`);
+
+      return httpRequest.json();
+    } catch (error) {
+      throw new Error(`Hue API Error: ${error.message}`);
+    }
+  }
 }
 
 export default Hue;
