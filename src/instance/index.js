a3bd0209474e183df1127837bfa2dc11550fc623
Matthew Taggart <matthew.taggart@cardcash.com>
2018-01-21 00:16:23 -0500

    Add function to get all lights from Hue api.

--- src/instance/index.js
+++ src/instance/index.js
@@ -1,7 +1,18 @@
+import fetch from 'node-fetch';
+
 class Hue {
   constructor(ip, username) {
     this.ip = ip;
     this.username = username;
+  }
+  async getAllLights() {
+    try {
+      const httpRequest = await fetch(`http://${this.ip}/api/${this.username}/lights`);
+
+      return httpRequest.json();
+    } catch (error) {
+      throw new Error(`Hue API Error: ${error.message}`);
+    }
   }
 }
 
